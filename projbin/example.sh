4816573283dbf5c465aa7139075401ac1454252f
Voigt <Stefan.Voigt@tui.com>
2019-11-28 11:26:41 +0100

    rename environment variable. better explanation in example.sh

--- projbin/example.sh
+++ projbin/example.sh
@@ -5,7 +5,7 @@ export SALT=0e4b4369-7c0f-4cb3-ae45-61966c756402
 export BUCKET=tuid-dv01-s3-kampdb
 
 # content specific
-export SDE=P
+export RTE=P
 export PROFILENAME=KampDB
 
 ###
@@ -13,10 +13,14 @@ export PROFILENAME=KampDB
 ###
 #
 ## prepare essential environment variables
-## better approach: do not source hardcoded variables but define as context, when running the container
+## better: do not source hardcoded variables but do one of following
+## a) define as context, when running the container
+## b) source from a file in /root/.projenv/
+## preferable in the entrypoint script 
 # source /projbin/example.sh  
 
-## add or update some credentials (user/login) with key P & snowflake (sde / profilename)
+## add or update some credentials (user/login) with key P & snowflake (RTE / profilename)
 # PROFILENAME=snowflake CREDUSER=snow CREDPWD=ichbineineschneeflocke /projbin/updatevault
+## dump vault
 # s3cmd get s3://${BUCKET}/${SALT} - | /projbin/dec $SALT
 
